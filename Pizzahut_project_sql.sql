e843a45fadefc25e350bb83d9d5f087baec19c84
Anuj sahu <anujjj.sahu1@gmail.com>
2026-07-30 00:31:04 +0530

    Add files via upload

---
+++ Pizzahut_project_sql.sql
@@ -0,0 +1,50 @@
+-- Retrieve the total number of orders placed.
+SELECT * FROM total_order;
+
+-- Calculate the total revenue generated from pizza sales.
+SELECT * from total_revenue;
+
+-- Identify the highest-priced pizza.
+SELECT * FROM highest_priced_pizza;
+
+-- Identify the most common pizza size ordered. 
+select * from pizza_size_ordered;
+
+-- List the top 5 most ordered pizza types along with their quantities.
+select * from 5_most_ordered_pizza;
+
+-- Join the necessary tables to find the total quantity of each pizza category ordered.
+select * from quantity_of_each_pizza;
+
+-- Determine the distribution of orders by hour of the day.
+select * from orders_by_hour;
+
+-- Join relevant tables to find the category-wise distribution of pizzas.
+select * from category_wise_distribution;
+
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+select * from pizzas_ordered_per_day;
+select * from orders_by_date;
+
+-- Determine the top 3 most ordered pizza types based on revenue.
+select * from 3_most_ordered_pizza;
+
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+select * from 3_most_ordered_pizza_types;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
